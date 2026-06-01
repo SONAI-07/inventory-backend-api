7c824bf6cbee02f17ba8ee4dd13271aa834dc589
Archanbanerjee <sonaibanerjee571@gmail.com>
2026-06-01 11:11:50 +0530

    Initial backend commit with Docker

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Allows any frontend to connect
+    allow_origins=["*"], # Allows any frontend to connecttouc
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
